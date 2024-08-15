22ac88e700f44dab73b19c636a3fa4033b394a44
ChristosKonstantas <titokonstantas1@gmail.com>
2024-08-15 19:29:48 +0300

    changed window text

--- HelloWorldWin/Source/MainComponent.cpp
+++ HelloWorldWin/Source/MainComponent.cpp
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::FontOptions (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setFont (juce::FontOptions (42.0f));
+    g.setColour (juce::Colours::red);
+    g.drawText ("Hello Jucy World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
