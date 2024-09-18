9a9ecf7a1cafbac38aec3db37a12322e7bf98f1c
ChristosKonstantas <titokonstantas1@gmail.com>
2024-09-18 11:54:04 +0300

    adding labels and sliders to visual front-end pt1

--- XY_Pad/Source/PluginEditor.h
+++ XY_Pad/Source/PluginEditor.h
@@ -29,5 +29,21 @@ private:
     // access the processor object that created it.
     XY_PadAudioProcessor& audioProcessor;
 
+    juce::Slider gainSlider {juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextBoxBelow };
+
+    juce::Slider panSlider{ juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextBoxBelow };
+
+    // An easy way to maintain state within the processor and the editor is to leverage apvts
+    // slider attachment s.t. there's synchronicity between the editor and the processor
+    // The value of the gain and the pan parameters could change directly from the editor
+    // when the user when the user moves the slider or it can change from some automation curve
+    // that the user has defined. Either way it will be reflected in both the processor and the
+    // editor so what we need to do is to attach these sliders to apvts
+    juce::AudioProcessorValueTreeState::SliderAttachment gainAttachment, panAttachment;
+
+    juce::Label gainLabel{ "gainLabel", "Gain" };
+    juce::Label panLabel{ "panLabel", "Pan" };
+
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (XY_PadAudioProcessorEditor)
 };
